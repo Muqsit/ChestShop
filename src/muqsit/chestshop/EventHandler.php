b189bc790311d5f35fd3132fc5f3096a5267590e
Adam Le <adamsuper169@gmail.com>
2020-01-24 07:13:57 -0800

    Revert

--- src/muqsit/chestshop/EventHandler.php
+++ src/muqsit/chestshop/EventHandler.php
@@ -44,7 +44,7 @@ class EventHandler implements Listener{
 		return true;
 	}
 
-	public function handleTransaction(Player $player, Item $itemClicked, Item $itemClickedUsing, SlotChangeAction $inventoryAction) : void{
+	public function handleTransaction(Player $player, Item $itemClicked, Item $itemClickedUsing, SlotChangeAction $inventoryAction) : bool{
 		$nbt = $itemClicked->getNamedTag();
 
 		if($nbt->hasTag("Button")){
