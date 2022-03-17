c357235bfe53a5819d0b0b3f11cfc3523331ce4a
Muqsit <hackhack05@gmail.com>
2022-03-17 17:44:54 +0000

    Remove BedrockEconomy warning

--- src/muqsit/chestshop/economy/BedrockEconomyIntegration.php
+++ src/muqsit/chestshop/economy/BedrockEconomyIntegration.php
@@ -9,10 +9,8 @@ use cooldogedev\BedrockEconomy\api\BedrockEconomyAPI;
 use cooldogedev\BedrockEconomy\BedrockEconomy;
 use cooldogedev\BedrockEconomy\libs\cooldogedev\libSQL\context\ClosureContext;
 use InvalidArgumentException;
-use muqsit\chestshop\Loader;
 use muqsit\chestshop\util\PlayerIdentity;
 use pocketmine\Server;
-use function assert;
 
 final class BedrockEconomyIntegration implements EconomyIntegration{
 
@@ -29,14 +27,6 @@ final class BedrockEconomyIntegration implements EconomyIntegration{
 	}
 
 	public function init(array $config) : void{
-		if(($config["unsafe-warn"] ?? true) && count($this->plugin->getConnector()->getThreads()) > 1){
-			/** @var Loader|null $chest_shop */
-			$chest_shop = Server::getInstance()->getPluginManager()->getPlugin("ChestShop");
-			assert($chest_shop !== null);
-			$chest_shop->getLogger()->warning("{$this->plugin->getName()} is unsafe to use with > 1 database threads");
-			$chest_shop->getLogger()->warning("To correct this issue, set 'threads' to 1 in {$this->plugin->getConfig()->getPath()}");
-			$chest_shop->getLogger()->warning("To suppress this error, set 'unsafe-warn' to false in {$chest_shop->getConfig()->getPath()}");
-		}
 	}
 
 	public function getMoney(PlayerIdentity $player, Closure $callback) : void{
@@ -50,26 +40,10 @@ final class BedrockEconomyIntegration implements EconomyIntegration{
 	}
 
 	public function removeMoney(PlayerIdentity $player, float $money, Closure $callback) : void{
-		/**
-		 * SAFETY NOTICE
-		 * This method executes two asynchronous processes simultaneously:
-		 * 		getPlayerBalance(), subtractFromPlayerBalance()
-		 *
-		 * It is completely possible for a race condition to happen and an unexpected value
-		 * be subtracted as a consequence.
-		 * BedrockEconomy lets player balances go below 0, so directly invoking
-		 * subtractFromPlayerBalance() is not possible.
-		 */
 		$money_int = (int) ceil($money);
-		$this->getMoney($player, static function(float $balance) use($player, $money_int, $callback) : void{
-			if($balance >= $money_int){
-				BedrockEconomyAPI::getInstance()->subtractFromPlayerBalance($player->getGamertag(), $money_int, ClosureContext::create(static function(bool $success) use($callback) : void{
-					$callback($success);
-				}));
-			}else{
-				$callback(false);
-			}
-		});
+		BedrockEconomyAPI::getInstance()->subtractFromPlayerBalance($player->getGamertag(), $money_int, ClosureContext::create(static function(bool $success) use($callback) : void{
+			$callback($success);
+		}));
 	}
 
 	public function formatMoney(float $money) : string{
