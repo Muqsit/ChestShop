ddc79708fc9349a4fbd7da10ece8b7252c2105cf
Muqsit <hackhack05@gmail.com>
2022-03-28 23:59:32 +0000

    Revert CategoryPage change since PocketMine treats "" item name as no name

--- src/muqsit/chestshop/category/CategoryPage.php
+++ src/muqsit/chestshop/category/CategoryPage.php
@@ -183,10 +183,7 @@ final class CategoryPage{
 		$find = ["{NAME}", "{COUNT}", "{PRICE}", "{PRICE_FORMATTED}", "{CATEGORY}", "{PAGE}"];
 		$replace = [$item->getName(), $item->getCount(), $entry->getPrice(), EconomyManager::get()->formatMoney($entry->getPrice()), $this->category->getName(), $this->page];
 
-		$name = str_replace($find, $replace, CategoryConfig::getString(CategoryConfig::ITEM_BUTTON_NAME));
-		if($name !== ""){
-			$item->setCustomName($name);
-		}
+		$item->setCustomName(str_replace($find, $replace, CategoryConfig::getString(CategoryConfig::ITEM_BUTTON_NAME)));
 		$lore = str_replace($find, $replace, CategoryConfig::getStringList(CategoryConfig::ITEM_BUTTON_LORE_VALUE));
 		switch(CategoryConfig::getString(CategoryConfig::ITEM_BUTTON_LORE_TYPE)){
 			case "push":
